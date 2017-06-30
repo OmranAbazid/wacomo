80c1335db2c57b3e8a564887c83ca96f10773a44
OmranAbazid <omran.1994@gmail.com>
2017-06-30 15:19:26 +0300

    adding wacomo-web

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var app = require('express')(); // Create an instance of an Express app
+var express = require('express');
+var app = express();
+var path = require('path');
 var mobileApp = require('azure-mobile-apps')();
 app.use(mobileApp);
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", function(req, res, err) {
+
     res.send("hello world");
+
 });
-app.get("/test", function(req, res, err) {
+
+app.get("/data", function(req, res, err) {
+
     var query = {            
         sql: 'Select * from wacomo',
                     parameters: []        
     };         
+
     req.azureMobile.data.execute(query).then(function(results) {            
         res.json(results);        
     });
+
 });
+
 app.listen(process.env.PORT || 3000);
