c7a2ce3e082958febd17c4f96bd3d9e99c236f90
OmranAbazid <omran.1994@gmail.com>
2017-06-30 14:18:40 +0300

    test

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var app = require('express')(); // Create an instance of an Express app
-
-app.get("/", function(req, res, err){
+var mobileApp = require('azure-mobile-apps')();
+app.use(mobileApp);
+app.get("/", function(req, res, err) {
     res.send("hello world");
 });
-
-
-
+app.get("/test", function(req, res, err) {
+    var query = {            
+        sql: 'Select * from wacomo',
+                    parameters: []        
+    };         
+    req.azureMobile.data.execute(query).then(function(results) {            
+        res.json(results);        
+    });
+});
 app.listen(process.env.PORT || 3000);
