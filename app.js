0ae75b849e1d41718da5f02ebb9621a11eecf5ea
OmranAbazid <omran.1994@gmail.com>
2017-06-30 14:00:32 +0300

    app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || 80, function () {
+  console.log('Example app listening on port 80!');
+});
